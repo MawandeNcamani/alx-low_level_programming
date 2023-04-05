03c4e96738be3e01cd9dd4d722599956ba8c27c3
MawandeNcamani <mawandencamani@gmail.com>
2023-04-05 12:39:01 -0700

    Task5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,21 +2,21 @@
 
 /**
 * free_listint2 - a function that frees a listint_t list, sets head to NULL
-* @head: a pointer to the head of the list
-* Return: returns nothing
+* @head_of: a pointer to the head of the list
+* Return: will return nothing
 */
-void free_listint2(listint_t **head)
+void free_listint2(listint_t **head_of)
 {
-	listint_t *tmp;
+	listint_t *temporary;
 
-	if (head == NULL)
+	if (head_of == NULL)
 		return;
 
-	while (*head != NULL)
+	while (*head_of != NULL)
 	{
-		tmp = *head;
-		*head = (*head)->next;
-		free(tmp);
+		temporary = *head_of;
+		*head_of = (*head_of)->next;
+		free(temporary);
 	}
-	*head = NULL;
+	*head_of = NULL;
 }
