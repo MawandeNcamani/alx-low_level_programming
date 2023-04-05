f516cdeef2ac51aa0dab152e6cf7a3e569ae024e
MawandeNcamani <mawandencamani@gmail.com>
2023-04-05 13:00:48 -0700

    Tasks 9 and 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -3,24 +3,24 @@
 /**
 * delete_nodeint_at_index - a function that deletes the node at
 * index of a listint_t linked list
-* @head: a pointer to the head of the list
+* @head_of: a pointer to the head of the list
 * @index: the index of the node to be deleted
 * Return: returns 1 if it succeeded, -1 if it failed
 */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+int delete_nodeint_at_index(listint_t **head_of, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *tmp = *head;
+	listint_t *tmp = *head_of;
 	listint_t *current = NULL;
 
-	 if (!(*head))
+	 if (!(*head_of))
 		return (-1);
 
 	 if (index == 0)
 	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
+		tmp = (*head_of)->next;
+		free(*head_of);
+		*head_of = tmp;
 		return (1);
 	}
 
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 
 /**
-* insert_nodeint_at_index - a function that inserts a new
+* insert_node_at_index - a function that inserts a new
 * node at a given position
-* @head: a pointer to the head of the linked list
-* @idx: the index of the list where the new node should be added.
+* @head_of: a pointer to the head of the linked list
+* @index: the index of the list where the new node should be added.
 * Index starts at 0
 * @n: the data to be added to the new node
 * Return: returns the address of the new node
 */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *insert_node_at_index(listint_t **head_of, unsigned int index, int n)
 {
 	listint_t *tmp;
 	listint_t *new;
 	unsigned int i = 0;
 
-	if (!head)
+	if (!head_of)
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
@@ -25,18 +25,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 
-	if (idx == 0)
+	if (index == 0)
 	{
-		new->next = (*head);
-		*head = new;
+		new->next = (*head_of);
+		*head_of = new;
 		return (new);
 	}
 
-	tmp = *head;
+	tmp = *head_of;
 
-	for (i = 0; tmp && i < idx; i++)
+	for (i = 0; tmp && i < index; i++)
 	{
-		if (i == idx - 1)
+		if (i == index - 1)
 		{
 			new->next = tmp->next;
 			tmp->next = new;
