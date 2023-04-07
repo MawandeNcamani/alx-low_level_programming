cda50a316e544ac1e61cc6d0d442eb4bd4f8f9f7
MawandeNcamani <mawandencamani@gmail.com>
2023-04-07 08:15:39 -0700

    Correction 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,16 +2,16 @@
 
 /**
 * get_bit - Returns the value of a bit at a given index
-* @n: The integer to be operated on
-* @index: The index
-* Return: The value of the bit at index or -1 on error
+* @num: The integer to be operated on
+* @ind: The index
+* Return: will return the value of the bit at index or -1 on error
 */
 
-int get_bit(unsigned long int n, unsigned int index)
+int get_bit(unsigned long int num, unsigned int ind)
 {
-if (index > 63)
+if (ind > 63)
 return (-1);
 
-int bit_value = ((n >> index) & 1);
+int bit_value = ((num >> ind) & 1);
 return (bit_value);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,9 +7,9 @@
 int _putchar(char c);
 unsigned int binary_to_uint(const char *y);
 void print_binary(unsigned long int num);
-int get_bit(unsigned long int num, unsigned int index);
-int set_bit(unsigned long int *num, unsigned int index);
-int clear_bit(unsigned long int *num, unsigned int index);
+int get_bit(unsigned long int num, unsigned int ind);
+int set_bit(unsigned long int *num, unsigned int ind);
+int clear_bit(unsigned long int *num, unsigned int ind);
 unsigned int flip_bits(unsigned long int num, unsigned long int m);
 int get_endianness(void);
 
