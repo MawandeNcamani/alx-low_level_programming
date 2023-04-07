9f0f8de54f17767084c572955a3134b8140d24bd
MawandeNcamani <mawandencamani@gmail.com>
2023-04-07 08:19:01 -0700

    Tasks

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,15 +3,15 @@
 /**
 * get_bit - Returns the value of a bit at a given index
 * @num: The integer to be operated on
-* @ind: The index
+* @index: The index
 * Return: will return the value of the bit at index or -1 on error
 */
 
-int get_bit(unsigned long int num, unsigned int ind)
+int get_bit(unsigned long int num, unsigned int index)
 {
-if (ind > 63)
+if (index > 63)
 return (-1);
 
-int bit_value = ((num >> ind) & 1);
+int bit_value = ((num >> index) & 1);
 return (bit_value);
 }
