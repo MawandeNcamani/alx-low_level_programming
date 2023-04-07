6722419b793254cb03ba17a92762a64963baad57
MawandeNcamani <mawandencamani@gmail.com>
2023-04-07 08:36:48 -0700

    Get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,16 +2,16 @@
 
 /**
 * get_bit - Returns the value of a bit at a given index
-* @num: The integer to be operated on
+* @n: The integer number to be operated on
 * @index: The index
 * Return: will return the value of the bit at index or -1 on error
 */
 
-int get_bit(unsigned long int num, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
 if (index > 63)
 return (-1);
 
-int bit_value = ((num >> index) & 1);
-return (bit_value);
+int value = ((n >> index) & 1);
+return (value);
 }
